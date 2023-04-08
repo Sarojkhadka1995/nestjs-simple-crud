adf6f3fc841a84e3792607a9b090628fcbc2baae
Saroj khadka <saroj.khadka@ekbana.info>
2023-04-08 17:01:33 +0545

    feat: Swagger add

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, { logger: console });
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
+  const config = new DocumentBuilder()
+    .setTitle('User microservice')
+    .setDescription('User microservice API description')
+    .setVersion('1.0')
+    .addBearerAuth(
+      {
+        name: 'Authorization',
+        bearerFormat: 'Bearer',
+        scheme: 'Bearer',
+        type: 'http',
+        in: 'Header',
+      },
+      'access-token',
+    )
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
